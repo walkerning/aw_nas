76fe3fa64b6181f893ae9b0f851f89787f1783d9
A-suozhang <574596610@qq.com>
2020-07-24 20:06:43 +0800

    fix some bugs

--- aw_nas/ops/bnn_ops.py
+++ aw_nas/ops/bnn_ops.py
@@ -17,13 +17,14 @@ from aw_nas.utils import make_divisible, drop_connect
 
 # The XNOR-Net
 
-class BinActive(torch.autograd.Function):
+class BinaryActivation(torch.autograd.Function):
+  @staticmethod
   def forward(self, input):
     self.save_for_backward(input)
     size = input.size()
     input = input.sign()
     return input
-  
+  @staticmethod
   def backward(self, grad_output):
     input, = self.saved_tensors
     grad_input = grad_output.clone()
@@ -48,7 +49,7 @@ class XNORConv2d(nn.Module):
     self.full_precision.data = self.full_precision.data - self.full_precision.data.mean(1, keepdim = True)
     self.full_precision.data.clamp_(-1, 1)
 
-    x = BinActive()(x)
+    x = BinaryActivation.apply(x)
     if self.dropout_ratio!=0:
       x = self.dropout(x)
     self.mean_val = self.full_precision.abs().view(self.out_channels, -1).mean(1, keepdim=True)
@@ -73,9 +74,9 @@ class XNORConv2d(nn.Module):
     self.full_precision.grad = binary_grad + mean_grad
     self.full_precision.grad = self.full_precision.grad * self.full_precision.data[0].nelement() * (1-1/self.full_precision.data.size(1))
 
-class BNConvReLU(nn.Module):
+class XNORBNConvReLU(nn.Module):
   def __init__(self, in_channels, out_channels, kernel_size, stride, padding, groups=1, dropout_ratio=0):
-    super(BNConvReLU, self).__init__()
+    super(XNORBNConvReLU, self).__init__()
     self.bn = nn.BatchNorm2d(in_channels, eps=1e-4, momentum=0.1, affine=True)
     self.bn.weight.zero_().add_(1.0)
     self.bn.bias.zero_()
@@ -104,7 +105,7 @@ class XNORLinear(nn.Module):
     self.full_precision.data = self.full_precision.data - self.full_precision.data.mean(1, keepdim = True)
     self.full_precision.data.clamp_(-1, 1)
 
-    x = BinActive()(x)
+    x = BinaryActivation.apply(x)
     if self.dropout_ratio != 0:
       x = self.dropout(x)
     self.mean_val = self.full_precision.abs().view(self.out_features, -1).mean(1, keepdim=True)
@@ -175,7 +176,8 @@ class BirealActivation(nn.Module):
         out1 = (-1) * mask1.type(torch.float32) + (x*x + 2*x) * (1-mask1.type(torch.float32))
         out2 = out1 * mask2.type(torch.float32) + (-x*x + 2*x) * (1-mask2.type(torch.float32))
         out3 = out2 * mask3.type(torch.float32) + 1 * (1- mask3.type(torch.float32))
-        out = out_forward.detach() - out3.detach() + out3
+        # out = out_forward.detach() - out3.detach() + out3
+        out = out_forward.detach() - x.detach() + x
 
         return out
 
@@ -192,20 +194,22 @@ class HardBinaryConv(nn.Module):
 
     def forward(self, x):
         real_weights = self.weights.view(self.shape)
-        scaling_factor = torch.mean(torch.mean(torch.mean(abs(real_weights),dim=3,keepdim=True),dim=2,keepdim=True),dim=1,keepdim=True)
-        #print(scaling_factor, flush=True)
-        scaling_factor = scaling_factor.detach()
-        binary_weights_no_grad = scaling_factor * torch.sign(real_weights)
+        scaling_factor = real_weights.abs().mean().reshape([1,1,1,1]).detach()
+        # scaling_factor = torch.mean(torch.mean(torch.mean(abs(real_weights),dim=3,keepdim=True),dim=2,keepdim=True),dim=1,keepdim=True)
+        # print(scaling_factor, flush=True)
+        # scaling_factor = scaling_factor.detach()
+        binary_weights_no_grad = scaling_factor * BinaryActivation.apply(real_weights)
         cliped_weights = torch.clamp(real_weights, -1.0, 1.0)
         binary_weights = binary_weights_no_grad.detach() - cliped_weights.detach() + cliped_weights
-        #print(binary_weights, flush=True)
+        # print(binary_weights, flush=True)
+        # binary_weights = BinaryActivation().apply(torch.clamp(real_weights, -1.0, 1.0))*scaling_factor # FIXME: concerns some problem with grad here
         y = F.conv2d(x, binary_weights, stride=self.stride, padding=self.padding, bias=self.bias)
 
         return y
 
 class BinaryConvBNReLU(nn.Module):
 
-    def __init__(self, C_in, C_out, kernel_size, stride, padding, affine,  bias=None, activation=torch.sign):
+    def __init__(self, C_in, C_out, kernel_size, stride, padding, affine,  bias=None, activation=BinaryActivation):
         super(BinaryConvBNReLU, self).__init__()
         self.bi_conv = HardBinaryConv(C_in, C_out, kernel_size=kernel_size,stride=stride, padding=padding, bias=bias, activation=activation)
         self.bi_act = activation
@@ -223,29 +227,93 @@ class BinaryConvBNReLU(nn.Module):
 
 
 class BinaryResNetBlock(nn.Module):
-    def __init__(self, C, C_out, stride, affine, kernel_size=3, act=torch.sign):
+    def __init__(self, C, C_out, stride, affine, kernel_size=3, act=BinaryActivation):
         super(BinaryResNetBlock, self).__init__()
         self.stride = stride
         padding = int((kernel_size - 1) / 2)
         self.activation = act
 
         self.op_1 = BinaryConvBNReLU(C, C_out,
-                               kernel_size,  stride, padding, affine=affine, bias=None, activation=self.activation)
+                               kernel_size, stride, padding, affine=affine, bias=None, activation=self.activation)
         self.op_2 = BinaryConvBNReLU(C_out, C_out,
                                kernel_size, 1, padding, affine=affine, bias=None, activation=self.activation)
+        # DBEUG: test correctness
+        # self.activation = Identity()
+        # self.op_1 = ConvBNReLU(C, C_out,
+        #                        kernel_size, stride, padding, affine=affine)
+        # self.op_2 = ConvBNReLU(C_out, C_out, 
+        #                        kernel_size, 1, padding, affine=affine)
         # the conv on skip-op is not binarized!
         self.skip_op = Identity() if stride == 1 else ConvBNReLU(C, C_out,
                                                                  1, stride, 0,
                                                                  affine=affine)
 
     def forward(self, inputs):
-        inner = self.activation(self.op_1(inputs))
-        out = self.op_2(inner)
-        out_skip = self.skip_op(inputs)
-        return self.activation(out + out_skip)
+        # for birealnet add an extra skip connection after op1
+        inner = self.op_1(inputs)+self.skip_op(inputs)
+        out = self.op_2(inner)+inner
+        return out
 
 register_primitive("bireal_resnet_block", 
                    lambda C, C_out, stride, affine: BinaryResNetBlock(C, C_out, stride=stride, affine=affine,act=BirealActivation()))
+
+class NIN(nn.Module):
+    def __init__(self):
+        super(NIN, self).__init__()
+        
+        self.conv1 = nn.Conv2d(3, 192, kernel_size = 5, stride = 1, padding = 2)
+        self.bn1 = nn.BatchNorm2d(192, eps=1e-4, momentum = 0.1, affine = False)
+
+        self.conv2_1 = BNConvReLU(192, 160, kernel_size=1, stride=1, padding=0)
+        self.conv2_2 = BNConvReLU(160, 96, kernel_size=1, stride=1, padding=0)
+
+        self.conv3_1 = BNConvReLU(96, 192, kernel_size=5, stride=1, padding=2, dropout_ratio=0.5)
+        self.conv3_2 = BNConvReLU(192, 192, kernel_size=1, stride=1, padding=0)
+        self.conv3_3 = BNConvReLU(192, 192, kernel_size=1, stride=1, padding=0)
+
+        self.conv4_1 = BNConvReLU(192, 192, kernel_size=3, stride=1, padding=1, dropout_ratio=0.5)
+        self.conv4_2 = BNConvReLU(192, 192, kernel_size=1, stride=1, padding=0)
+
+        self.bn5 = nn.BatchNorm2d(192, eps = 1e-4, momentum = 0.1, affine = False)
+        self.conv5 = nn.Conv2d(192, 10, kernel_size = 1, stride = 1, padding = 0)
+
+        for m in self.modules():
+            if isinstance(m, nn.BatchNorm2d) or isinstance(m, nn.BatchNorm1d):
+                if hasattr(m.weight, 'data'):
+                    m.weight.data.zero_().add_(1.0)
+                    
+    def forward(self, x, params, flops):
+        for m in self.modules():
+            if isinstance(m, nn.BatchNorm2d) or isinstance(m, nn.BatchNorm1d):
+                if hasattr(m.weight, 'data'):
+                    m.weight.data.clamp_(min = 0.01)
+                    
+        x = F.relu(self.bn1(self.conv1(x)))
+        params = params + self.conv1.weight.nelement()
+        flops = flops + self.conv1.weight.nelement() * 5 * 5 * x.size(-1) * x.size(-2)
+
+        x, params, flops = self.conv2_1(x, params, flops)
+        x, params, flops = self.conv2_2(x, params, flops)
+        x = F.max_pool2d(x, kernel_size = 3, stride = 2, padding = 1)
+
+        x, params, flops = self.conv3_1(x, params, flops)
+        x, params, flops = self.conv3_2(x, params, flops)
+        x, params, flops = self.conv3_3(x, params, flops)
+        x = F.avg_pool2d(x, kernel_size=3, stride=2, padding=1)
+
+        x, params, flops = self.conv4_1(x, params, flops)
+        x, params, flops = self.conv4_2(x, params, flops)
+
+        x = F.relu(self.conv5(self.bn5(x)))
+        params = params + self.conv5.weight.nelement()
+        flops = flops + self.conv5.weight.nelement() * 1 * 1 * x.size(-1) * x.size(-2)
+        x = F.avg_pool2d(x, kernel_size = 8)
+        x = x.squeeze()
+        return x, params, flops
+
+
+
+
 
 # class BiRealNet(nn.Module):
 # 
