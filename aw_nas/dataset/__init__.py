8e2628326763f790f60d741e7f0c44d7beafa2f3
DiDiDoes <chengdicao@foxmail.com>
2021-07-11 15:22:36 +0800

    [Transferable NAS] support Mini-ImageNet dataset

--- aw_nas/dataset/__init__.py
+++ aw_nas/dataset/__init__.py
@@ -10,6 +10,7 @@ from aw_nas.dataset import imagenet
 from aw_nas.dataset import tiny_imagenet
 from aw_nas.dataset import cifar100
 from aw_nas.dataset import svhn
+from aw_nas.dataset import miniimagenet
 
 try:
     from aw_nas.dataset import voc
